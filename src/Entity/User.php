674734bac1837c38ecfafc7dc373a6c1d2ba2c29
Splagadou <benoit@vandanjon.fr>
2023-01-02 14:40:29 +0100

    add getUsers group in User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
@@ -15,36 +16,46 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups("getUsers")]
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
+    #[Groups("getUsers")]
     private ?string $email = null;
 
     #[ORM\Column]
+    #[Groups("getUsers")]
     private array $roles = [];
 
     /**
      * @var string The hashed password
      */
     #[ORM\Column]
+    #[Groups("getUsers")]
     private ?string $password = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups("getUsers")]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups("getUsers")]
     private ?string $lastName = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups("getUsers")]
     private ?string $userName = null;
 
     #[ORM\ManyToMany(targetEntity: Session::class, inversedBy: 'users')]
+    #[Groups("getUsers")]
     private Collection $session;
 
     #[ORM\ManyToMany(targetEntity: Language::class, inversedBy: 'users')]
+    #[Groups("getUsers")]
     private Collection $language;
 
     #[ORM\OneToMany(mappedBy: 'user', targetEntity: ContactLink::class, orphanRemoval: true)]
+    #[Groups("getUsers")]
     private Collection $contactLink;
 
     public function __construct()
